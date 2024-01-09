824d298e55a3860ed34aeabe6a2823f8e0cba20b
Jung Yunho <a01049048063@gmail.com>
2024-01-08 16:18:18 -0800

    feat: setting project

---
+++ src/main/java/com/example/memic/MemicApplication.java
@@ -0,0 +1,13 @@
+package com.example.memic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemicApplication.class, args);
+    }
+
+}
