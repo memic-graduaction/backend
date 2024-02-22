b5993db0a57287f20796183be98255b21da214d1
Jung Yunho <a01049048063@gmail.com>
2024-02-22 23:05:46 +0900

    feat: cors 설정파일 추가

---
+++ src/main/java/com/example/memic/common/config/web/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.example.memic.common.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
