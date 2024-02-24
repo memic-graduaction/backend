d4838cce86539e9462715f67e75a933625a764ed
Jung Yunho <a01049048063@gmail.com>
2024-02-24 21:57:56 +0900

    feat: Get, Post 허용하도록 변경

--- src/main/java/com/example/memic/common/config/web/WebMvcConfig.java
+++ src/main/java/com/example/memic/common/config/web/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST")
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(3600);
