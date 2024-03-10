8a1713654af1ea88527e4a99c8b98283e2bbd702
bjgraduation <20160320@kookmin.ac.kr>
2024-03-10 17:07:16 +0900

    test: yt-dlp에 의존적인 테스트를 빌드시 무시하도록 설정

--- src/test/java/com/example/memic/service/YoutubeMp4ExtractServiceTest.java
+++ src/test/java/com/example/memic/service/YoutubeMp4ExtractServiceTest.java
@@ -3,13 +3,15 @@ package com.example.memic.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.memic.transcription.application.YoutubeMp4Extractor;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("NonAsciiCharacters")
 class YoutubeMp4ExtractServiceTest {
 
+    @Disabled("yt-dlp가 설치되어 있어야 합니다.")
     @Test
-    void 유튜브_비디오_추출() throws Exception {
+    void 유튜브_비디오_추출() {
         final var service = new YoutubeMp4Extractor("/Users/jung-yunho/youtube-mp3/", "yt-dlp");
         String output = service.extractVideo("https://youtu.be/lpcpsCY4Mco?si=_rxzxxH-fuE78HDf");
         assertThat(output).contains("100%");
