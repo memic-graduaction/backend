2ca1945e70d9ed3c1ddbd1902c1e0cee89157b56
Jung Yunho <a01049048063@gmail.com>
2024-05-05 15:11:53 +0900

    test: tag부분에 member 관련 로직 추가

--- src/test/java/com/example/memic/phrase/ui/TagIntegrationTest.java
+++ src/test/java/com/example/memic/phrase/ui/TagIntegrationTest.java
@@ -8,6 +8,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.example.memic.common.EnableMockMvc;
 import com.example.memic.common.database.NoTransactionExtension;
+import com.example.memic.member.dto.MemberSignInRequest;
+import com.example.memic.member.dto.MemberSignInResponse;
 import com.example.memic.phrase.dto.TagCreateRequest;
 import com.example.memic.phrase.dto.TagCreateResponse;
 import com.example.memic.phrase.dto.TagListResponse;
@@ -25,38 +27,54 @@ import org.springframework.test.web.servlet.MockMvc;
 @SuppressWarnings("NonAsciiCharacters")
 @EnableMockMvc
 @SpringBootTest
+@ExtendWith(NoTransactionExtension.class)
 class TagIntegrationTest {
 
     @Autowired
-    private ObjectMapper objectmapper;
+    private ObjectMapper objectMapper;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     @ExtendWith(NoTransactionExtension.class)
-    void 태그를_저장하고_목록을_가져온다() throws Exception{
+    void 태그를_저장하고_목록을_가져온다() throws Exception {
+        final var memberSignInRequest = new MemberSignInRequest("test@gmail.com", "123");
+
+        final var loginResult = mockMvc.perform(post("/v1/members/sign-up")
+                                               .contentType(MediaType.APPLICATION_JSON)
+                                               .content(objectMapper.writeValueAsString(memberSignInRequest)))
+                                       .andDo(print())
+                                       .andExpect(status().isOk())
+                                       .andReturn();
+        final var memberSignInResponse = objectMapper.readValue(loginResult.getResponse().getContentAsString(),
+                MemberSignInResponse.class);
+
         final var request = new TagCreateRequest("테스트");
 
         final var responseFromPost = mockMvc.perform(post("/v1/tags")
-                        .content(objectmapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").isNumber())
-                .andReturn();
+                                                    .header("Authorization", memberSignInResponse.accessToken())
+                                                    .content(objectMapper.writeValueAsString(request))
+                                                    .contentType(MediaType.APPLICATION_JSON))
+                                            .andDo(print())
+                                            .andExpect(status().isOk())
+                                            .andExpect(jsonPath("$.id").isNumber())
+                                            .andReturn();
 
-        final var tagCreateResponse = objectmapper.readValue(
+        final var tagCreateResponse = objectMapper.readValue(
                 responseFromPost.getResponse().getContentAsString(),
                 TagCreateResponse.class);
 
-        final var responseFromGet = mockMvc.perform(get("/v1/tags"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andReturn();
+        final var responseFromGet = mockMvc.perform(get("/v1/tags")
+                                                   .header("Authorization", memberSignInResponse.accessToken()))
+                                           .andDo(print())
+                                           .andExpect(status().isOk())
+                                           .andReturn();
 
-        TypeReference<List<TagListResponse>> listType = new TypeReference<>(){};
-        final var tagListResponse = objectmapper.readValue(responseFromGet.getResponse().getContentAsString(), listType);
+        TypeReference<List<TagListResponse>> listType = new TypeReference<>() {
+        };
+        final var tagListResponse = objectMapper.readValue(responseFromGet.getResponse().getContentAsString(),
+                listType);
 
         SoftAssertions.assertSoftly(softly -> {
             softly.assertThat(tagCreateResponse.id()).isEqualTo(tagListResponse.get(0).id());
