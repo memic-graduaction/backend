cb62f8c136e44504dffd0d88fcdf0db4c114bd59
Jung Yunho <a01049048063@gmail.com>
2024-05-11 21:54:59 +0900

    refactor: HttpMethod 메서드를 사용하도록 수정

--- src/main/java/com/example/memic/common/auth/AuthInterceptor.java
+++ src/main/java/com/example/memic/common/auth/AuthInterceptor.java
@@ -4,6 +4,7 @@ import com.example.memic.common.exception.InvalidTokenException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -20,7 +21,7 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        if (request.getMethod().equals("OPTIONS")) {
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
             return true;
         }
 
