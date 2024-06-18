7eeac80e3eb0fdab50c0d10123ba2191be7ff1a1
Yunho Jung <87285536+yunhobb@users.noreply.github.com>
2024-06-18 19:41:25 +0900

    feat: 사용자 최신 조회 동영상 목록을 조회 (#57)
    * feat: 사용자 최신 조회 동영상 목록을 조회
    
    * fix: final 인자 제거

---
+++ src/main/java/com/example/memic/transcription/dto/TranscriptionRecentListResponse.java
@@ -0,0 +1,16 @@
+package com.example.memic.transcription.dto;
+
+import com.example.memic.transcription.domain.Transcription;
+
+public record TranscriptionRecentListResponse(
+        Long id,
+        String url
+) {
+
+    public static TranscriptionRecentListResponse fromEntity(Transcription transcription) {
+        return new TranscriptionRecentListResponse(
+                transcription.getId(),
+                transcription.getUrl()
+        );
+    }
+}
--- src/test/java/com/example/memic/transcription/ui/TranscriptionIntegrationTest.java
+++ src/test/java/com/example/memic/transcription/ui/TranscriptionIntegrationTest.java
@@ -14,6 +14,7 @@ import com.example.memic.member.dto.MemberSignInRequest;
 import com.example.memic.member.dto.MemberSignInResponse;
 import com.example.memic.transcription.dto.SentenceResponse;
 import com.example.memic.transcription.dto.TranscriptionCreateRequest;
+import com.example.memic.transcription.dto.TranscriptionRecentListResponse;
 import com.example.memic.transcription.dto.TranscriptionResponse;
 import com.example.memic.transcription.dto.TranscriptionUrlListResponse;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -21,6 +22,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Comparator;
 import java.util.List;
 import javax.sql.DataSource;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -130,5 +132,57 @@ class TranscriptionIntegrationTest {
                 listType);
 
         assertEquals(transcriptionUrlListResponse.size(), 3);;
+    }
+
+    @Test
+    @ExtendWith(NoTransactionExtension.class)
+    void 사용자가_최근_조회한_동영상_목록을_반환한다() throws Exception{
+        try (final var connection = dataSource.getConnection()) {
+            connection.createStatement().execute("SET FOREIGN_KEY_CHECKS=0");
+            connection.createStatement().execute("INSERT INTO member (id, email, password) VALUES (1, 'yunho@naver.com', '1234')");
+            connection.createStatement().execute("INSERT INTO transcription (id, url, transcribed_at) VALUES (1, 'https://test01.com',  '2024-06-04 12:34:56')");
+            connection.createStatement().execute("INSERT INTO transcription (id, url, transcribed_at) VALUES (2, 'https://test02.com',  '2024-06-05 12:34:56')");
+            connection.createStatement().execute("INSERT INTO transcription (id, url, transcribed_at) VALUES (3, 'https://test03.com',  '2024-06-06 12:34:56')");
+            connection.createStatement().execute("INSERT INTO transcription (id, url, transcribed_at) VALUES (4, 'https://test04.com',  '2024-06-07 12:34:56')");
+            connection.createStatement().execute("INSERT INTO transcription_member(id, transcription_id, member_id) VALUES (1, 1, 1)");
+            connection.createStatement().execute("INSERT INTO transcription_member(id, transcription_id, member_id) VALUES (2, 2, 1)");
+            connection.createStatement().execute("INSERT INTO transcription_member(id, transcription_id, member_id) VALUES (3, 3, 1)");
+            connection.createStatement().execute("INSERT INTO transcription_member(id, transcription_id, member_id) VALUES (4, 4, 1)");
+            connection.createStatement().execute("INSERT INTO transcription_member(id, transcription_id, member_id) VALUES (5, 4, 2)");
+        }
+
+        final var signInRequest = new MemberSignInRequest("yunho@naver.com", "1234");
+
+
+        final var mvcSignInResponse = mockMvc.perform(post("/v1/members/sign-in")
+                                                     .contentType(MediaType.APPLICATION_JSON)
+                                                     .content(objectMapper.writeValueAsString(signInRequest)))
+                                             .andDo(print())
+                                             .andExpect(status().isOk())
+                                             .andReturn();
+
+        final var signInResponse = objectMapper.readValue(
+                mvcSignInResponse.getResponse().getContentAsString(),
+                MemberSignInResponse.class
+        );
+
+        final var responseFromGet = mockMvc.perform(get("/v1/transcriptions/recent/all")
+                                                   .header("Authorization", signInResponse.accessToken()))
+                                           .andDo(print())
+                                           .andExpect(status().isOk())
+                                           .andReturn();
+
+        TypeReference<List<TranscriptionRecentListResponse>> listType = new TypeReference<>() {
+        };
+        final var transcriptionRecentListResponses = objectMapper.readValue(responseFromGet.getResponse().getContentAsString(),
+                listType);
+
+        SoftAssertions.assertSoftly(
+                softly ->{
+                    softly.assertThat(transcriptionRecentListResponses).hasSize(4);
+                    softly.assertThat(transcriptionRecentListResponses)
+                          .isSortedAccordingTo(Comparator.comparing(TranscriptionRecentListResponse::id).reversed());
+                }
+        );
     }
 }
