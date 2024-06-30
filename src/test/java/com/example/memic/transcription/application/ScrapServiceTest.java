c830799670806ed61c8efe8736b910d9311dcc6c
Jung Yunho <a01049048063@gmail.com>
2024-06-30 21:20:24 +0900

    feat: fetch 조건을 lazy로 변경

--- src/test/java/com/example/memic/transcription/application/ScrapServiceTest.java
+++ src/test/java/com/example/memic/transcription/application/ScrapServiceTest.java
@@ -34,17 +34,11 @@ class ScrapServiceTest {
     @Autowired
     private MemberRepository memberRepository;
 
-//    @Autowired
-//    private TransactionTemplate transactionTemplate;
-//
-//    @Autowired
-//    private PlatformTransactionManager platformTransactionManager;
 
 
     @BeforeEach
     void init() {
-//        transactionTemplate = new TransactionTemplate(platformTransactionManager);
-//        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+
         Member member = new Member("yunho@naver.com", "1234");
         memberRepository.save(member);
         Transcription transcription = new Transcription("https://", Map.of(LocalTime.now(), "hello"));
@@ -53,29 +47,30 @@ class ScrapServiceTest {
 
 
     @Test
-    void 일반_테스트() {
+    void 스크랩을_저장한다() {
+        //given
         Member member = memberRepository.findByEmail("yunho@naver.com").get();
         ScrapCreateRequest request = new ScrapCreateRequest(1L);
+
+        //when
         for (int i = 0; i < 10; i++) {
             scrapService.createScrap(member, request);
         }
-        System.out.println("############");
-        System.out.println(scrapRepository.findAllByMember(member).size());
-        System.out.println("############");
+        //then
         assertEquals(1, scrapRepository.findAllByMember(member).size());
     }
 
     @Test
-    void 동시성_테스트() throws InterruptedException {
+    void 병렬_환경에서_스크랩을_저장한다() throws InterruptedException {
+        //given
         Member member = memberRepository.findByEmail("yunho@naver.com").get();
         ScrapCreateRequest request = new ScrapCreateRequest(1L);
+        int numberOfThread = 50;
 
-        //given
-        ExecutorService threadPool = Executors.newFixedThreadPool(50);
-        CountDownLatch countDownLatch = new CountDownLatch(50);
+        ExecutorService threadPool = Executors.newFixedThreadPool(numberOfThread);
+        CountDownLatch countDownLatch = new CountDownLatch(numberOfThread);
 
-        for (int i = 0; i < 50; i++) {
-//            transactionTemplate.executeWithoutResult((transactionStatus -> {
+        for (int i = 0; i < numberOfThread; i++) {
             threadPool.execute(() -> {
                 try {
                     scrapService.createScrap(member, request);
@@ -85,15 +80,10 @@ class ScrapServiceTest {
                     countDownLatch.countDown();
                 }
             });
-//            }));
         }
         countDownLatch.await();
 
         //then
-        System.out.println("############");
-        System.out.println(scrapRepository.findAllByMember(member).size());
-        System.out.println("############");
-
         assertEquals(1, scrapRepository.findAllByMember(member).size());
     }
 }
